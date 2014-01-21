e387b0fc63ff2d45d2e234a9e423ca7e4fb6fe5f
sapo93 <sapo93@gmail.com>
2014-01-21 18:05:56 +0100

    Updated test.lisp

--- ool.lisp
+++ ool.lisp
@@ -1,1 +1,7 @@
 ;;;; Surname Name ID
+
+(defparameter *classes-specs* (make-hash-table))
+(defparameter *slot-value*)
+
+(define-class (class-name parent slot-value)
+	
--- test.lisp
+++ test.lisp
@@ -1,1 +1,51 @@
 ;;;; Test File
+
+;;;; Loading File With OOL API
+(load "ool.lisp")
+
+
+;;;; Definition Of A Class Person
+(define-class 'person nil 'name "Eve" 'age "undefined")
+
+
+;;;; Definition Of A Class Students, Subclass Of Person
+(define-class 'student 'person 
+  'name "Eva Lu Ator"
+  'university "Berkeley"
+  'talk '(method ()
+	  (princ "My name is ")
+	  (princ (get-slot this 'name))
+	  (terpri)
+	  (princ "My age is ")
+	  (princ (get-slot this 'age))))
+
+
+;;;; Istances Of Class Person
+(defparameter *eve* (new 'person))
+(defparameter *adam* (new 'person 'name "Adam"))
+
+
+;;;; Instances Of Class Student
+(defparameter *s1* (new 'student 
+			'name "Eduardo De Filippo"
+			'age 108))
+
+(defparameter *s2* (new 'student))
+
+
+;;;; Istances Ispection
+(get-slot eve 'age)
+
+(get-slot s2 'name)
+
+(get-slot eve 'adress)
+
+
+;;;; Methods Calling
+(talk eve)
+
+(talk adam)
+			
+
+	      
+  
