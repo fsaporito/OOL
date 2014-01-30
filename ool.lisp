fca8179aacece30f1b7c082500df9f66d673fd08
Francesco Saporito <sapo93@gmail.com>
2014-01-30 17:01:50 +0100

    Update ool.lisp

--- ool.lisp
+++ ool.lisp
@@ -21,14 +21,24 @@
 
 
 
+;;;; Method-Process
+;;;; Function Used To Process Methods, Rewriting Them To
+;;;; Lisp Functions 
+(defun method-process (method-name method-spec)
+	(error "Not Implemented Yet"))
+	
+	
+
 ;;;; Input Checking For The Slots
 (defun check-slot (slot-values)
   (if (null slot-values) ;; slot-values Is Empty, Return True
       T
       (if (symbolp (first slot-values)) ;; The First Element Must Be A Symbol
-	  (check-slot (rest (rest slot-values))) ;; Check From The Third Element
-	  Nil)
-      Nil))
+	  (if (equal (car (second slot-values)) 'method )
+	      ((method-process (first slot-values) (second slot-values))
+	       (check-slot (rest (rest slot-values)))	      
+	      (check-slot (rest (rest slot-values))) ;; Check From Third Element
+	  Nil)))
 
 
 
@@ -44,10 +54,11 @@
 
 
 
+;;;; Define-Class
 ;;;; Function Used To Define A New Class
-(defun define-class (class-name parent slot-values)
+(defun define-class (class-name parent &rest slot-values)
   (if (input-class-check class-name parent slot-values)  
-      (if (get-class-spec class-name) ;; Checks If The Class Is Already Defined
+      (if (not (get-class-spec class-name)) ;; Check If Class Is Already Defined
 	  (if (null parent) ;; Parent Class If Exists Must Be Defined
 	      (add-class-spec class-name (append (list parent) slot-values))
 	      (if (not (null (get-class-spec parent)))
@@ -60,7 +71,7 @@
 
 ;;;; New 
 ;;;; Instantiate A New Class Previously Defined
-(defun new (class-name slot-values)
+(defun new (class-name &rest slot-values)
   (if (symbolp class-name) 
       (if (get-class-spec class-name)
 	  (if (check-slot slot-values)
@@ -68,6 +79,7 @@
 	      (error "Wrong Slot Values"))
 	  (error "Class Not Defined !!!"))
       (error "Wrong Class Name")))
+      
 
 
 
@@ -80,18 +92,19 @@
 	  (second list) ;; Return Slot-Value
 	  (find-slot (rest (rest list)) slot-name)) ;; Recursive Step
       Nil))
+      
 
 
 
 ;;;; Find Slot Parent
 ;;;; This Function Find The Slot-Name Recursively
 ;;;; In The Parents. Nil If Not Found
-(defun find-slot-parent (parent slot-name)
+(defun find-slot-p (parent slot-name)
   (if (not (null parent)) ;; Parent  Mustn't Be Nil
       (if (not (null (get-class-spec parent)))
 	  (if (not (null (find-slot (get-class-spec parent) slot-name)))
 	      (find-slot (get-class-spec parent) slot-name)
-	      (find-slot-parent (first (get-class-spec parent)) slot-name))
+	      (find-slot-p (first (get-class-spec parent)) slot-name))
 	  Nil)
       Nil))
       
@@ -104,16 +117,14 @@
   (if (and (not (null instance)) (symbolp slot-name))
       (if (not (null (find-slot (rest instance) slot-name))) 
 	  (find-slot (rest instance) slot-name)
-	  (let (class-sp (get-class-spec (first instance)))
+	  (let ((class-sp (get-class-spec (first instance))))
 	    (if (not (null class-sp))
 		(if (not (null (find-slot (rest class-sp) slot-name)))
-		    (find-slot (rest class-sp))
+		    (find-slot (rest class-sp) slot-name)
 		    (if (not (null (find-slot-p (first class-sp) slot-name)))
 			(find-slot-p (first class-sp) slot-name)
-			(error "Slot Not Found"))
-		  (error "Slot Not Found"))
-		(error "Slot Not Found")))
-	  (error "Slot Not Found"))
+			(error "Slot Not Found")))
+		(error "Slot Not Found"))))
       (error "Wrong Input")))
 
 
